90a7bf9807059af8e0c2c72a22b784d7a1b74682
ARSCRY <sergy897@gmail.com>
2024-07-29 16:58:02 -0500

    Update diagramaDispersion.r
    cambios hechos en la tabla.

--- Graficos/diagramaDispersion.r
+++ Graficos/diagramaDispersion.r
@@ -5,7 +5,7 @@ x2 <- c(2, 2, 8, 1, 15, 8, 12, 9, 7, 3, 11, 4, 7, 14, 12)
 y2 <- c(100, 105, 84, 105, 90, 99, 90, 95, 94, 100, 79, 112, 91, 80, 85)
 
 
-plot(x, y, main = "Observacion de carros", 
+plot(x, y, main = "Observacion de carros obtenidos en R> ", 
 xlab = "Antigüedad", ylab = "Velocidad", col = "red", cex = 2)
 
 points(x2, y2, col = "blue", cex = 2)
